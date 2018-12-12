0e126fd08dc2e51ff13d59c15027b35ba105555f
Jesse Clark <jjclark1982@gmail.com>
2018-12-12 22:02:04 +1100

    add 'reloader' harness to run 'start', 'stop', 'update' methods

---
+++ example_program.py
@@ -0,0 +1,11 @@
+import time
+
+def start():
+    print("starting")
+
+def stop():
+    print("exiting")
+
+def update():
+    print("updating", 13)
+    time.sleep(0.5)
---
+++ reloader.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import importlib
+import os
+from datetime import datetime, timedelta
+import atexit
+
+import update as program
+
+stat_time = datetime.now()
+old_mtime = os.path.getmtime(program.__file__)
+def reload_program():
+    global stat_time, old_mtime
+
+    if datetime.now() - stat_time < timedelta(0,1): # one second
+        return
+
+    stat_time = datetime.now()
+    new_mtime = (os.path.getmtime(program.__file__))
+    if new_mtime > old_mtime:
+        print("Change detected. Reloading {}".format(program.__file__))
+        old_mtime = new_mtime
+        try:
+            importlib.reload(program)
+        except Exception as e:
+            print(e)
+
+def stop():
+    if getattr(program, 'stop'):
+        program.stop()
+
+def run():
+    if getattr(program, 'start'):
+        program.start()
+    while True:
+        reload_program()
+        try:
+            program.update()
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    atexit.register(stop)
+    run()
